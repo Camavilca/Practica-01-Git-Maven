8300e00cdd76ecef274fde8690efafe4af07d058
CAMAVILCA <orlando.camavilca@tecsup.edu.pe>
2018-09-13 10:58:22 -0500

    PRIMER COMMIT CAMAVILCA

---
+++ src/main/java/com/camavilca/practica/camavilca/gitmaven/practica01_camavilca_martinez_gitmaven/App.java
@@ -0,0 +1,13 @@
+package com.camavilca.practica.camavilca.gitmaven.practica01_camavilca_martinez_gitmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
